3155d198bfd53567358c2e504de8caa4a8973afc
Wen.Liao <wen@skymizer.com>
2021-05-17 11:15:23 +0800

    Update install packages

--- scripts/init_ubuntu_20.04.sh
+++ scripts/init_ubuntu_20.04.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-sudo apt update && sudo apt install aptitude && sudo aptitude upgrade -y --full-resolve
+sudo apt update && sudo apt install -y aptitude && sudo aptitude upgrade -y --full-resolve
 
-sudo apt install -y build-essential bison flex automake libtool intltool  libncurses5-dev git-svn tig meld cscope vim-gtk fonts-inconsolata geany joe doxygen-gui exuberant-ctags manpages-dev manpages-posix-dev minicom tree terminator ttf-mscorefonts-installer ack-grep pandoc gnome-system-tools ghex mc dict wireshark mtr pdfgrep sshfs fail2ban rkhunter bc libssl-dev cmake gdb libatk-adaptor libgail-common
+sudo apt install -y build-essential bison flex automake libtool intltool  libncurses5-dev git-svn tig meld cscope vim-gtk fonts-inconsolata geany doxygen-gui exuberant-ctags manpages-dev manpages-posix-dev minicom tree terminator ack-grep pandoc gnome-system-tools ghex mc dict wireshark mtr pdfgrep sshfs fail2ban rkhunter bc libssl-dev cmake gdb libatk-adaptor libgail-common linux-image-generic-hwe-20.04 linux-headers-generic-hwe-20.04 gcin-anthy libpython3-all-dev
+
+
+
+
